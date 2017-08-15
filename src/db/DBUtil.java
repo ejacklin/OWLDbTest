01417ebe6c9ad7545a783d4b9a2fe0d4b8652bc7
Erin Jacklin <ejacklin2@gmail.com>
2017-08-14 22:34:50 -0400

    Almost there!

--- src/db/DBUtil.java
+++ src/db/DBUtil.java
@@ -1,4 +1,4 @@
-package com.DBTest;
+package db;
 
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
---
+++ src/db/UserDB.java
@@ -0,0 +1,38 @@
+package db;
+
+
+import javax.persistence.EntityManager;
+
+/**
+ * Created by Erin on 8/14/2017.
+ */
+public class UserDB {
+    public static UserEntity getUserById(Integer id){
+        EntityManager em = DBUtil.getEmFactory().createEntityManager();
+        try{
+            UserEntity user = em.find(UserEntity.class, id);
+            return user;
+        }finally {
+            em.close();
+        }
+    }
+
+    public static SessionEntity getSessionById(Integer session_id){
+        EntityManager em = DBUtil.getEmFactory().createEntityManager();
+        try{
+            SessionEntity session = em.find(SessionEntity.class, session_id);
+            return session;
+        }finally {
+            em.close();
+        }
+    }
+
+    public static void main(String[] args){
+        UserDB db = new UserDB();
+
+//        Session s = db.getSessionById(2);
+        UserEntity u = db.getUserById(6);
+        System.out.println(u.getFirstName());
+//        System.out.println(s.getDate());
+    }
+}
